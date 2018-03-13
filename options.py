37dd8536a241d0e0ea802803ffc4ad27749070e4
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-12 15:25:33 -1000

    Update options.py

--- options.py
+++ options.py
@@ -3,4 +3,5 @@ import bs4 as bs
 
 response = requests.get('https://www.nasdaq.com/symbol/aapl/option-chain?money=all&dateindex=6')
 soup = bs.BeautifulSoup(response.text, 'lxml')
-print(soup)
+table = soup.findAll('table')[5]
+print(table)
