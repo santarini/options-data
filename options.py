fd3455ed913d84332e9ab51c1e4da402586f7786
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-12 15:57:48 -1000

    Update options.py

--- options.py
+++ options.py
@@ -1,21 +1,20 @@
 import requests
 import bs4 as bs
+import csv
 
 response = requests.get('https://www.nasdaq.com/symbol/aapl/option-chain?money=all&dateindex=6')
 soup = bs.BeautifulSoup(response.text, 'lxml')
-table = soup.findAll('table')[5]
-for row in table.findAll('tr')[1:]:
-    expiry = row.findAll('td')[0].text
-    last = row.findAll('td')[1].text
-    chg = row.findAll('td')[2].text
-    bid = row.findAll('td')[3].text
-    ask = row.findAll('td')[4].text
-    vol = row.findAll('td')[5].text
-    openInt = row.findAll('td')[6].text
-    print(expiry)
-    print(last)
-    print(chg)
-    print(bid)
-    print(ask)
-    print(vol)
-    print(openInt)
+calltable = soup.findAll('table')[5]
+with open('option.csv', 'a') as csvfile:
+    fieldnames = ['Expiry','Last','Change','Bid','Ask','Vol','Open Interest']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, lineterminator = '\n')
+    writer.writeheader()
+    for row in calltable.findAll('tr')[1:]:
+        expiry = row.findAll('td')[0].text
+        last = row.findAll('td')[1].text
+        chg = row.findAll('td')[2].text
+        bid = row.findAll('td')[3].text
+        ask = row.findAll('td')[4].text
+        vol = row.findAll('td')[5].text
+        openInt = row.findAll('td')[6].text
+        writer.writerow({'Expiry': expiry,'Last': last,'Change': chg,'Bid': bid,'Ask': ask,'Vol': vol,'Open Interest': openInt})
