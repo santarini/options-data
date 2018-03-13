fffe3547b2a0430552f73fecc9f7735e1aba7791
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-12 16:03:58 -1000

    Update options.py

--- options.py
+++ options.py
@@ -2,11 +2,13 @@ import requests
 import bs4 as bs
 import csv
 
-response = requests.get('https://www.nasdaq.com/symbol/aapl/option-chain?money=all&dateindex=6')
+ticker = "aapl"
+
+response = requests.get('https://www.nasdaq.com/symbol/' + ticker + '/option-chain?money=all&dateindex=6')
 soup = bs.BeautifulSoup(response.text, 'lxml')
 calltable = soup.findAll('table')[5]
 with open('option.csv', 'a') as csvfile:
-    fieldnames = ['Expiry','Last','Change','Bid','Ask','Vol','Open Interest']
+    fieldnames = ['Ticker', 'Expiry','Last','Change','Bid','Ask','Vol','Open Interest']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames, lineterminator = '\n')
     writer.writeheader()
     for row in calltable.findAll('tr')[1:]:
@@ -17,4 +19,4 @@ with open('option.csv', 'a') as csvfile:
         ask = row.findAll('td')[4].text
         vol = row.findAll('td')[5].text
         openInt = row.findAll('td')[6].text
-        writer.writerow({'Expiry': expiry,'Last': last,'Change': chg,'Bid': bid,'Ask': ask,'Vol': vol,'Open Interest': openInt})
+        writer.writerow({'Ticker': ticker.upper(),'Expiry': expiry,'Last': last,'Change': chg,'Bid': bid,'Ask': ask,'Vol': vol,'Open Interest': openInt})
