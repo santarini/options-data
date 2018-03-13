2a96328d8aed212dd9a7473760e5c442083cf896
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-12 16:44:21 -1000

    Update optiondates.py

--- optiondates.py
+++ optiondates.py
@@ -6,4 +6,8 @@ import csv
 response = requests.get('https://www.nasdaq.com/symbol/aapl/option-chain')
 soup = bs.BeautifulSoup(response.text, 'lxml')
 optiondatecount = soup.find("div", {"id": "OptionsChain-dates"}).text
+optiondatecount = optiondatecount.lstrip()
 print(optiondatecount)
+optiondates = optiondatecount.replace(" |  ", ",")
+myList = optiondates.split(",")
+print(myList)
