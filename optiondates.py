0394388fb955ad7bc2973b786f104d3e4db10059
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-18 13:39:18 -1000

    Update optiondates.py

--- optiondates.py
+++ optiondates.py
@@ -8,10 +8,12 @@ soup = bs.BeautifulSoup(response.text, 'lxml')
 optiondatestring = soup.find("div", {"id": "OptionsChain-dates"}).text
 optiondatestring = optiondatestring.lstrip()
 optiondatestring = optiondatestring.replace(" |  ", ",")
+optiondatestring = optiondatestring.replace(" ", "_20")
 optionDateList = optiondatestring.split(",")
 optionDateFullList = []
 i = 0
 for x in optionDateList:    
     optionDateFullList.append([x,i])
     i +=1
+print(optionDateList)
 print(optionDateFullList)
